f3231f68771d54b151dad9d4c52ec56f07c5c7c9
drumnation <davidmieloch@gmail.com>
2017-05-06 13:21:02 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
-  
+
   // browser refreshes on submit
-  $('form').on('submit', function(){
-    var name = $('#name').val();
-    $("#hello").text("Hello, " + name);
-  });
+  // $('form').on('submit', function() {
+  //   var name = $('#name').val()
+  //   $("#hello").text("Hello, " + name)
+  //   event.preventDefault()
+  // })
 
   //examine event object
-  // $('form').on('submit', function(event){
-  //   var name = $('#name').val();
-  //   $("#hello").text("Hello, " + name);
-  //   debugger;
-  //   event.preventDefault();
-  // });
+  $('form').on('submit', function(event){
+    var name = $('#name').val();
+    $("#hello").text("Hello, " + name);
+    debugger;
+    event.preventDefault();
+  });
 
   // stop page refresh
   // $('form').on('submit', function(event){
